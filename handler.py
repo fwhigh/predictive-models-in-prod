0ec82b0e5c9e02e565e0c083c5229a112c07e196
fwhigh <fwhigh@gmail.com>
2019-01-13 21:49:55 -0800

    Works but query parameter must be supplied to /predict

---
+++ handler.py
@@ -0,0 +1,108 @@
+try:
+    import unzip_requirements
+except ImportError:
+    pass
+
+import json
+import logging
+
+from pmip.data import load_from_s3_and_unpickle, get_latest_s3_dateint
+
+model_id = get_latest_s3_dateint(datadir='models', bucket='fwhigh-predictive-models')
+model = load_from_s3_and_unpickle(filename='model.pkl', subdirectory=f'models/{model_id}', bucket='fwhigh-predictive-models')
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
+
+def healthcheck(event, context):
+    body = {
+        "status": "ok"
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
+
+def predict(event, context):
+    request_data = json.loads(event['body'])
+
+    if 'queryStringParameters' in event:
+        if 'flavor' in event['queryStringParameters']:
+            flavor = event['queryStringParameters']['flavor']
+    else:
+        flavor = None
+
+    if 'comments' not in request_data:
+        logging.error("You must provide a list of comments in the data payload")
+        raise Exception("You must provide a list of comments in the data payload")
+        return
+
+    if flavor is None or flavor == 'class':
+        val = model.predict(request_data['comments'])
+        prediction = [{'class': int(s)} for s in val]
+    elif flavor == 'probability':
+        val = model.predict_proba(request_data['comments'])
+        prediction = [{'probability': list(s)} for s in val]
+    else:
+        logging.error(f"Unknown type {flavor}")
+        raise Exception(f"Unknown type {flavor}")
+        return
+
+    body = {
+        'prediction': prediction
+    }
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(body)
+    }
+
+    return response
+
+
+def get_test(event, context):
+    val = model.predict(["Check it out this free stuff!!!", "I take issue with your characterization."])
+    prediction = [{'class': int(s)} for s in val]
+
+    body = {'prediction': prediction}
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(body)
+    }
+
+    return response
--- pmip/data.py
+++ pmip/data.py
@@ -1,5 +1,71 @@
 import os
 import pickle
+import re
+
+import boto3
+import botocore
+
+
+def get_s3_keys(bucket, Prefix=None):
+    """Get a list of keys in an S3 bucket."""
+    client = boto3.client('s3')
+
+    keys = []
+    resp = client.list_objects_v2(Bucket=bucket, MaxKeys=100000, Prefix=Prefix)
+    if 'Contents' in resp:
+        for obj in resp['Contents']:
+            keys.append(obj['Key'])
+    return keys
+
+
+def get_latest_s3_dateint(datadir=None, bucket=None):
+    all_keys = get_s3_keys(bucket, Prefix=datadir)
+    latest_dateint = None
+    for subdir in all_keys:
+        m = re.search(datadir + r'/(\d{8})', subdir)
+        if m:
+            dateint = m.group(1)
+            if latest_dateint is None or dateint > latest_dateint:
+                latest_dateint = dateint
+    if latest_dateint is None:
+        raise RuntimeError("No dateint directories found in {}/{}/".format(bucket, datadir))
+    return latest_dateint
+
+
+def load_from_s3(filename=None, subdirectory=None, bucket=None):
+    response = None
+
+    if filename is None:
+        raise ValueError("You must specify a filename")
+    if subdirectory is None:
+        raise ValueError("You must specify a subdirectory")
+
+    client = boto3.client('s3')
+
+    try:
+        response = client.get_object(Bucket=bucket, Key=subdirectory + '/' + filename)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The obj does not exist.")
+        else:
+            raise
+
+    obj_bytes = response['Body'].read()
+
+    return obj_bytes
+
+
+def load_from_s3_and_unpickle(filename=None, subdirectory=None, bucket=None):
+    if filename is None:
+        raise ValueError("You must specify a filename")
+    if subdirectory is None:
+        raise ValueError("You must specify a subdirectory")
+
+    obj_bytes = load_from_s3(filename=filename, subdirectory=subdirectory, bucket=bucket)
+
+    obj = pickle.loads(obj_bytes)
+
+    return obj
 
 
 def pickle_to_fs(obj=None, filename=None, subdirectory=None):
@@ -8,28 +74,13 @@ def pickle_to_fs(obj=None, filename=None, subdirectory=None):
     if filename is None:
         raise ValueError("You must specify a filename")
     if subdirectory is None:
-        subdirectory = "."
+        raise ValueError("You must specify a subdirectory")
 
     if not os.path.exists(subdirectory):
         os.makedirs(subdirectory)
 
     with open(os.path.join(subdirectory, filename), 'wb') as pickle_file:
         pickle.dump(obj, pickle_file)
-
-
-def load_from_fs_and_unpickle(filename=None, subdirectory=None):
-    if filename is None:
-        raise ValueError("You must specify a filename")
-    if subdirectory is None:
-        subdirectory = "."
-
-    if not os.path.exists(subdirectory):
-        raise ValueError('Directory {} does not exist'.format(subdirectory))
-
-    with open(os.path.join(subdirectory, filename), 'rb') as pickle_file:
-        obj = pickle.load(pickle_file)
-
-    return obj
 
 
 def download_glove():
