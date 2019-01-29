76b1e0067126baaf8ce7f9727cc4f38c78abdd1f
fwhigh <fwhigh@gmail.com>
2019-01-29 08:41:49 -0800

    Add region env variable

--- scripts/build_training_image.sh
+++ scripts/build_training_image.sh
@@ -3,6 +3,7 @@
 docker build \
     --build-arg AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) \
     --build-arg AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) \
+    --build-arg AWS_DEFAULT_REGION=$(aws configure get region) \
     --build-arg GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_PERSONAL_ACCESS_TOKEN \
     --build-arg ENVIRONMENT=$ENVIRONMENT \
     -f Dockerfile.train -t pmip:latest .
